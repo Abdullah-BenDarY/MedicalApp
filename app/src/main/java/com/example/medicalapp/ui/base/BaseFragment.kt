2107a50a4720a1f24267720fcc3f023e1142e779
AbdullahBendary <abdullahbendary7@gmail.com>
2024-08-18 04:56:13 +0300

    fix: base fragment memory leak issue

--- app/src/main/java/com/example/medicalapp/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/medicalapp/ui/base/BaseFragment.kt
@@ -22,8 +22,8 @@ abstract class BaseFragment <VB : ViewBinding>(private val bindingInflater : (in
 
     open fun onClicks(){}
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 
